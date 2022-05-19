bbe64902ed03fdfbe2b5ffa7ba55872dbad3e64f
Hyla Fourie <1835707@students.wits.ac.za>
2022-05-19 18:33:32 +0200

    again

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path = require('path')            // used later in the exercise
  app.get('/', function (req, res) {
  res.send('Hello World')
 })
+app.get('/Public', function(req, res){
+    res.sendFile(path.join(__dirname, 'View', 'SinglePlayer.html'));
+    });
+    
 
 const port = process.env.PORT || 3000
 app.listen(port)
